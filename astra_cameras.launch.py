1a37dfda6a76da6fa26d9487069fd948ce02cf8a
ASTRA-SHC <astra@uah.edu>
2024-05-24 00:31:03 -0500

    Update launch file

--- astra_cameras.launch.py
+++ astra_cameras.launch.py
@@ -11,15 +11,20 @@ CAMERAS_LIST = [
         'name': 'camera0',
         'params': 'params_0.yaml'
     }
+,
+    {
+        'name': 'camera1',
+        'params': 'params_1.yaml'
+    }
+,
+    {
+        'name': 'camera2',
+        'params': 'params_2.yaml'
+    }
 #,
 #    {
-#        'name': 'camera1',
-#        'params': 'params_1.yaml'
-#    }
-#,
-#    {
-#        'name': 'camera2',
-#        'params': 'params_2.yaml'
+#	'name': 'camera3'
+#	'params': 'params_3.yaml'
 #    }
     # Add more Camera's here and they will automatically be launched below
 ]
@@ -34,39 +39,17 @@ def generate_launch_description():
     for camera in CAMERAS_LIST:
         nodes_list.append(
             Node(
-                package='v4l2_camera_custom', executable='v4l2_camera_node', output='screen',
-                name=f"v4l2_{camera['name']}", # Node Name
-                namespace=camera['name'], # Namespace?
+                package='v4l2_camera', executable='v4l2_camera_node', output='screen',
+                name=f"v4l2_camera_node", # Node Name
+                namespace=f"/core", # Namespace, modify for each module
                 parameters=[
                     Path().resolve() / camera['params'],
                 ],
                 remappings=[
-                    ("__ns", f"/{camera['name']}") # What is the main output topic for the camera?
                     # ("") # What is the compressed output topic for the camera?
                 ]
             )
         )
-        # Republisher code, to be moved to the basestation
-        # Speak to Alex R about this if you have questions
-        """ nodes_list.append(
-            Node(
-                package='image_transport', executable='republish', output='screen',
-                name=f"republisher_{camera['name']}",
-                # namespace=camera['name'],
-                # remappings=[
-                    # ("__ns", f"/{camera['name']}")
-                    # ("/in", f"/{camera['name']}/image_raw/compressed"),
-                    # ("/out/compressed", f"{camera['name']}/compressed/republished")
-                # ],
-                # compressed /in/compressed:=/camera1/image_raw/compressed raw /out:=/compressed_republished
-                arguments=[
-                    "compressed",
-                    f"/in/compressed:=/{camera['name']}/image_raw/compressed",
-                    "raw",
-                    f"/out:=/{camera['name']}/republished/compressed"
-                ]
-            )
-        ) """
 
     nodes_groupaction = GroupAction(nodes_list)
 
